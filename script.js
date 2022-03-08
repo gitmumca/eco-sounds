96b0dea1af2698c0bed7ac14e6e28979b3849d5c
gitmumca <gitmumca@mail.ru>
2022-03-08 23:15:19 +0300

    feat: add burger

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+﻿// открытие меню бублика
+
+const menu = document.getElementById('menu-toggle-nav');
+menu.addEventListener('click', function() {
+	const box = document.querySelector('.menu-box');
+	box.style.visibility = (box.style.visibility == 'visible') ? 'hidden' : 'visible';
+})
+
+// закрытие меню бублика после нажатия на строки меню
+
+const items = document.querySelectorAll('.menu-item');
+items.forEach(el => el.addEventListener('click', function() {
+	document.querySelector('.menu-box').style.visibility = 'hidden';
+	document.getElementById('menu-toggle-nav').checked = false;
+}))
+
 //---- звуки
 
   var isPlay = false;
